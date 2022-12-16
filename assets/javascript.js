51939d9fc95ae11da9f7d53ed17abe9c10d3ea86
SamsonKnightz <113211927+SamsonKnightz@users.noreply.github.com>
2022-12-15 21:05:08 -0500

    added display function

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,21 +1,33 @@
-var APIKey = "371dead36e8c5b8aa978441fa4daca9a"
 
-var getDrink = function () {
-    var liquor = $('#liquor option:selected').val();
-    localStorage.setItem("liquor", liquor);
-    var APIKey = '71dead36e8c5b8aa978441fa4daca9a' + liquor;
+
+var getCity = function () {
+    var city = $('#city').val();
+    localStorage.setItem("city", city);
+    var APIKey = "https://api.openweathermap.org/data/2.5/weather?q="+ city + "&appid=" + APIKey + "&units=imperial";
     fetch(APIKey)
-        .then(function (response) {
-            if (response.ok) {
-                response.json().then(function (data) {
-                    var drinks = data.drinks;
-                    displayDrink(drinks);
-                });
-            } else {
-                alert('Error: ' + response.statusText);
-            }
+    .then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                var city = data.city;
+                displayCity(city);
+            });
+        } else {
+            alert('Error: ' + response.statusText);
+        }
         })
         .catch(function (error) {
             alert('Unable to connect');
         });
 };
+
+var displayMovie = function (data) {
+    var movieName = data.Title;
+    $('#movieTitle').text(movieName);
+    var moviePoster = data.Poster;
+    $("#moviePic").attr("src", moviePoster)
+    var moviePlot = data.Plot;
+    $('#moviePlot').text(moviePlot);
+    var movieId = data.imdbID;
+    var movieLink = "https://www.imdb.com/title/" + movieId;
+    $("#movieLink").attr("href", movieLink)
+}
