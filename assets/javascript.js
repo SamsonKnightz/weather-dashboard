6afdef9d30487cc9c8492821900407f3dbdcd207
SamsonKnightz <113211927+SamsonKnightz@users.noreply.github.com>
2022-12-18 18:40:57 -0500

    linked button to fetch request

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,10 +1,11 @@
 var APIKey = "371dead36e8c5b8aa978441fa4daca9a"
-
+var city = ""
+https://api.openweathermap.org/data/2.5/weather?q=Charlotte&appid=371dead36e8c5b8aa978441fa4daca9a&units=imperial
 var getCity = function () {
     var city = $('#city').val();
     localStorage.setItem("city", city);
-    var APIKey = "https://api.openweathermap.org/data/2.5/weather?q="+ city + "&appid=" + APIKey + "&units=imperial";
-    fetch(APIKey)
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+ city + "&appid=" + APIKey + "&units=imperial";
+    fetch(apiUrl)
     .then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
@@ -20,9 +21,9 @@ var getCity = function () {
         });
 };
 
-var displayMovie = function (data) {
-    var movieName = data.Title;
-    $('#movieTitle').text(movieName);
+var displayCity = function (data) {
+    var cityName = data.Title;
+    $('#cityTitle').text(cityName);
     var moviePoster = data.Poster;
     $("#moviePic").attr("src", moviePoster)
     var moviePlot = data.Plot;
@@ -30,4 +31,9 @@ var displayMovie = function (data) {
     var movieId = data.imdbID;
     var movieLink = "https://www.imdb.com/title/" + movieId;
     $("#movieLink").attr("href", movieLink)
-}
+};
+
+$("#init").click(function(event){
+    event.preventDefault();
+    getCity();
+})
